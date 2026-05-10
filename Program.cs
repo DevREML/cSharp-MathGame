773f595a582678b50837f274b666423eb3f6fe94
Raissa-1104080 <raissa_limon@hotmail.com>
2026-05-10 16:01:13 +0200

    Pseudocode

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Variables 
+
+/* Menu
+Where the player can (1) choose an operation 
+and look up the history of previous game. 
+
+While (player chooses not to quit te game) 
+    show menu 
+    wait for input
+*/
+
+/* Game
+if (player choose the operation option)
+    for loop (with a minimum of 5 questions):
+    ask question
+    collect input 
+    check answer (if correct: score +1) 
+    
+    if (game ends > 5 questions have been answered)
+        show score
+        save score to List of History
+    
+if (player chooses history):
+    show previous games.         
+*/
